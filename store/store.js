7cd88e6155b6bf0781b456de145dd9ef55d8faf6
Ankit Yadav <122344948+LiveWithCodeAnkit@users.noreply.github.com>
2024-10-08 09:44:44 +0530

    Update store.js

--- store/store.js
+++ store/store.js
@@ -3,7 +3,7 @@ import campaignSlice from "./brief_builder/campaign/campaign.slice";
 import campaignRequestSlice from "./campaign_request/campaignRequest.slice";
 import chatSokcetSlice from "./chat_scoket/chatSokcetSlice";
 import appSlice from "./chat_scoket/appSlice";
-
+//https://github.com/SahilHirpara45/claim-ms-portal
 export const store = configureStore({
   reducer: {
     Campaign: campaignSlice,
